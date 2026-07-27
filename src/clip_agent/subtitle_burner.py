3c7a349adb4832e246cd501ceb31cbd87b99e367
wangzibo <wangzibo@users.noreply.github.com>
2026-07-27 22:44:53 +0800

    refine: ASS字幕SimHei备选字体

--- src/clip_agent/subtitle_burner.py
+++ src/clip_agent/subtitle_burner.py
@@ -56,6 +56,7 @@ PlayResY: 1920
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
 Style: Default,Microsoft YaHei,{font_size},&H00FFFFFF,&H000000FF,&H80000000,&H00000000,-1,0,0,0,100,100,0,0,1,3,2,2,50,50,30,1
+Style: SimHei,SimHei,{font_size},&H00FFFFFF,&H000000FF,&H80000000,&H00000000,-1,0,0,0,100,100,0,0,1,3,2,2,50,50,30,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
